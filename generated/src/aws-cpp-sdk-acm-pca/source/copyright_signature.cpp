75415b56bbeea93171b550af16973abcf2c61ac3
hulken <hulkas@gmail.com>
2024-05-21 08:47:35 +0200

    Update copyright_signature.cpp

--- generated/src/aws-cpp-sdk-acm-pca/source/copyright_signature.cpp
+++ generated/src/aws-cpp-sdk-acm-pca/source/copyright_signature.cpp
@@ -1,4 +1,4 @@
-namespace ACM_PCA {
+namespace ACM_PCA_EELDEV {
 
 #ifdef _WIN32
 #define DLL_EXPORT __declspec(dllexport)
@@ -9,4 +9,4 @@ namespace ACM_PCA {
     DLL_EXPORT const char* _COPYRIGHT_NOTICE_ACM_PCA = "Copyright (c) 2023 eelDev AB. All rights reserved.";
     DLL_EXPORT const char* _COPYRIGHT_NOTICE_ACM_PCA_EELDEV = "Library compiled for eelDev AB by eelDev AB.";
 
-} // namespace ACM_PCA
+} // namespace ACM_PCA_EELDEV
