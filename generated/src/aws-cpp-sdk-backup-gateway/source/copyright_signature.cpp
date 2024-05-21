9a8cc3793ab6243d28b78287bfaa37084e8e7272
hulken <hulkas@gmail.com>
2024-05-21 08:28:47 +0200

    added copyright files

---
+++ generated/src/aws-cpp-sdk-backup-gateway/source/copyright_signature.cpp
@@ -0,0 +1,12 @@
+namespace REPLACE_ME {
+
+#ifdef _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+    DLL_EXPORT const char* _COPYRIGHT_NOTICE = "Copyright (c) 2023 eelDev AB. All rights reserved.";
+    DLL_EXPORT const char* _COPYRIGHT_NOTICE_EELDEV = "Library compiled for eelDev AB by eelDev AB.";
+
+} // namespace REPLACE_ME
---
+++ generated/src/aws-cpp-sdk-compute-optimizer/source/copyright_signature.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace REPLACE_ME {
+
+#ifdef _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    DLL_EXPORT extern const char* _COPYRIGHT_NOTICE;
+    DLL_EXPORT extern const char* _COPYRIGHT_NOTICE_EELDEV;
+
+#ifdef __cplusplus
+}
+#endif
+
+} // namespace REPLACE_ME
